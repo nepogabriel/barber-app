200ccd21446a5a9d137b97ff324b23623280f980
Gabriel Ribeiro <gabrielribeiroonline@gmail.com>
2023-12-02 17:49:41 -0300

    Dev - Colunas de horário e cliente criadas na tabela de agendamento

--- app/Http/Controllers/site/OrderController.php
+++ app/Http/Controllers/site/OrderController.php
@@ -68,7 +68,7 @@ class OrderController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        
     }
 
     /**
--- resources/views/site/order/index.blade.php
+++ resources/views/site/order/index.blade.php
@@ -5,36 +5,37 @@
         <table class="table table-sm">
             <tbody>
               <tr>
-                <td>Data:</td>
+                <td class="fw-bold">Data:</td>
                 <td>{{ $order['hour'][0]->date }}</td>
+                <input type="hidden" name="order_date" value="{{ $order['hour'][0]->date }}"/>
               </tr>
               <tr>
-                <td>Horário:</td>
+                <td class="fw-bold">Horário:</td>
                 <td>{{ $order['hour'][0]->time }}</td>
               </tr>
 
               <tr>
-                <td>Profissional:</td>
+                <td class="fw-bold">Profissional:</td>
                 <td>{{ $order['professional'][0]->name }}</td>
               </tr>
 
               <tr>
-                <td>Serviço:</td>
+                <td class="fw-bold">Serviço:</td>
                 <td>{{ $order['service'][0]->name }}</td>
               </tr>
 
               <tr>
-                <td>Valor:</td>
+                <td class="fw-bold">Valor:</td>
                 <td>R$&nbsp;{{ $order['service'][0]->price }}</td>
               </tr>
               
               <tr>
-                <td>Cliente:</td>
+                <td class="fw-bold">Cliente:</td>
                 <td>{{ $order_session['name_client'] }}</td>
               </tr>
 
               <tr>
-                <td>Tel. Cliente:</td>
+                <td class="fw-bold">Tel. Cliente:</td>
                 <td>{{ $order_session['telephone_client'] }}</td>
               </tr>
             </tbody>
