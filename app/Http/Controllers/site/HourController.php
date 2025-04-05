2c509972940b650520a3b1d5b06b90899b2358a5
nepogabriel <nepogabriel16@gmail.com>
2025-04-05 13:42:59 -0300

    PSA-12 - Adicionando exceção, log e mensagem amigável para usuário final quando o mesmo selecionar algum horário e avançar no fluxo

--- app/Http/Controllers/site/HourController.php
+++ app/Http/Controllers/site/HourController.php
@@ -9,6 +9,7 @@ use App\Services\HourControlService;
 use App\Services\HourService;
 use App\Services\ServiceService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class HourController extends Controller
 {
@@ -34,23 +35,34 @@ class HourController extends Controller
 
     public function store(HourFormRequest $request)
     {
-        $ids_hour_control_selected = $request->session()->get('order.ids_hour_control') ?: [];
+        try {
+            $ids_hour_control_selected = $request->session()->get('order.ids_hour_control') ?: [];
 
-        $alert_user = $this->hour_control_service->validateHourControl($request->hour_id, $ids_hour_control_selected);
+            $alert_user = $this->hour_control_service->validateHourControl($request->hour_id, $ids_hour_control_selected);
 
-        if (isset($alert_user['alert_user']) && $alert_user['alert_user']) {
-            $message_alert_user = $alert_user['message'] ?: 'Desculpe! Outro usuário escolheu o mesmo horário.';
+            if (isset($alert_user['alert_user']) && $alert_user['alert_user']) {
+                $message_alert_user = $alert_user['message'] ?: 'Desculpe! Outro usuário escolheu o mesmo horário.';
+
+                return to_route('site.hour.index')
+                    ->with('hour_control.alert_user', $message_alert_user); 
+            }
+
+            $this->hour_control_service->hourControl($request->hour_id);
+
+            $hours_id = array_map('intval', $request->hour_id);
+            $request->session()->put('order.hour_id', $hours_id);
+
+            return to_route('site.client.index');
+        } catch (\Exception $exception) {
+            Log::critical('Aconteceu algo inesperado com os horários.', [
+                'key' => 'log_hour',
+                'message' => $exception->getMessage(),
+                'code' => $exception->getCode(),
+            ]);
 
             return to_route('site.hour.index')
-                ->with('hour_control.alert_user', $message_alert_user); 
+                ->with('hour_control.alert_user', 'Ops! Aconteceu algo inesperado, tente novamente ou mais tarde.');
         }
-
-        $this->hour_control_service->hourControl($request->hour_id);
-
-        $hours_id = array_map('intval', $request->hour_id);
-        $request->session()->put('order.hour_id', $hours_id);
-
-        return to_route('site.client.index');
     }
 
     public function show(Request $request)
