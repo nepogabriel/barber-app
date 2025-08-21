fcb55b52b92f54ad7445ce127db7a85911f0936f
nepogabriel <nepogabriel16@gmail.com>
2025-08-21 18:54:37 -0300

    PSA-14 - Adicionando tratamento de exceções para pegar o erro de produção

--- app/Http/Controllers/site/ClientController.php
+++ app/Http/Controllers/site/ClientController.php
@@ -5,24 +5,51 @@ namespace App\Http\Controllers\site;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\site\ClientFormRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ClientController extends Controller
 {
     public function index(Request $request)
     {
-        $name_client = $request->session()->get('order.name_client');
-        $telephone_client = $request->session()->get('order.telephone_client');
+        try {
+            $name_client = $request->session()->get('order.name_client') ?? '';
+            $telephone_client = $request->session()->get('order.telephone_client') ?? '';
 
-        return view('site.client.index')
-            ->with('name_client', $name_client)
-            ->with('telephone_client', $telephone_client);
+            return view('site.client.index')
+                ->with('name_client', $name_client)
+                ->with('telephone_client', $telephone_client);   
+        } catch (\Exception $exception) {
+            Log::critical('Aconteceu algo inesperado ao resgatar dados do usuário na sessão.', [
+                'key' => 'log_client',
+                'message' => $exception->getMessage(),
+                'code' => $exception->getCode(),
+            ]);
+
+            return to_route('site.start.index')
+                ->with('message.order_success', 'Ops! Aconteceu algo inesperado, tente novamente ou mais tarde.');
+        }
     }
 
     public function store(ClientFormRequest $request)
     {
-        $request->session()->put('order.name_client', $request->name_client);
-        $request->session()->put('order.telephone_client', $request->telephone_client);
+        try {
+            if (isset($request->name_client) && isset($request->telephone_client)) {
+                $request->session()->put('order.name_client', $request->name_client);
+                $request->session()->put('order.telephone_client', $request->telephone_client);
 
-        return to_route('site.order.index');
+                return to_route('site.order.index');
+            }
+
+            return view('site.client.index');
+        } catch (\Exception $exception) {
+            Log::critical('Aconteceu algo inesperado ao salvar dados do usuário na sessão.', [
+                'key' => 'log_client',
+                'message' => $exception->getMessage(),
+                'code' => $exception->getCode(),
+            ]);
+
+            return to_route('site.start.index')
+                ->with('message.order_success', 'Ops! Aconteceu algo inesperado, tente novamente ou mais tarde.');
+        }
     }
 }
--- app/Http/Controllers/site/OrderController.php
+++ app/Http/Controllers/site/OrderController.php
@@ -87,9 +87,21 @@ class OrderController extends Controller
 
     public function show(Request $request)
     {
-        $message_order_success = $request->session()->get('message.order_success');
+        try {
+            $message_order_success = $request->session()->get('message.order_success');
 
-        return view('site.order.show')
-            ->with('message', $message_order_success);
+            return view('site.order.show')
+                ->with('message', $message_order_success);
+
+        } catch (\Exception $exception) {
+            Log::critical('Aconteceu algo inesperado ao mostrar confirmação do agendamento.', [
+                'key' => 'log_appointment',
+                'message' => $exception->getMessage(),
+                'code' => $exception->getCode(),
+            ]);
+
+            return view('site.order.index')
+                ->with('message_alert_user', 'Ops! Aconteceu algo inesperado, tente novamente ou mais tarde.');
+        }
     }
 }
