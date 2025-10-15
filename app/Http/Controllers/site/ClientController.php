32ed46726fdb9f09f896dedc7dd5a702d7daa9c8
nepogabriel <nepogabriel16@gmail.com>
2025-10-15 20:50:02 -0300

    PSA-15 - Utilizando parâmetro para mostrar ou não 'resumo do agendamento' no fluxo de agendar serviço

--- app/Http/Controllers/site/ClientController.php
+++ app/Http/Controllers/site/ClientController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers\site;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\site\ClientFormRequest;
+use App\Http\Service\SettingService;
+use App\Services\AppointmentService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
 class ClientController extends Controller
 {
+    public function __construct(
+        private AppointmentService $appointment_service,
+    ) {}
+
     public function index(Request $request)
     {
         try {
@@ -33,14 +39,29 @@ class ClientController extends Controller
     public function store(ClientFormRequest $request)
     {
         try {
-            if (isset($request->name_client) && isset($request->telephone_client)) {
-                $request->session()->put('order.name_client', $request->name_client);
-                $request->session()->put('order.telephone_client', $request->telephone_client);
+            if (!isset($request->name_client) && !isset($request->telephone_client)) {
+                return view('site.client.index');
+            }
 
+            $request->session()->put('order.name_client', $request->name_client);
+            $request->session()->put('order.telephone_client', $request->telephone_client);
+            
+            $settingService = new SettingService();
+            
+            if ($settingService->getSetting(SettingService::CODE_GENERAL . '_order_summary')) {
                 return to_route('site.order.index');
             }
 
-            return view('site.client.index');
+            $data = [
+                'orders' => $request->session()->get('order.hour_id'),
+                'professional_id' => $request->session()->get('order.professional_id'),
+                'name_client' => $request->name_client,
+                'telephone_client' => $request->telephone_client,
+            ];
+
+            $this->appointment_service->createAppointments($data);
+
+            return to_route('site.order.show');
         } catch (\Exception $exception) {
             Log::critical('Aconteceu algo inesperado ao salvar dados do usuário na sessão.', [
                 'key' => 'log_client',
